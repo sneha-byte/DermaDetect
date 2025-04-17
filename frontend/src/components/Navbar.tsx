c9546b3332b52c6cfd44fbffbf6b95cf7e5cc5cd
Sneha Sadhwani <snehasadhwani@hotmail.com>
2025-04-17 10:44:35 -0700

    increased font for header

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -12,25 +12,25 @@ function Navbar() {
             <div className="w-full flex justify-baseline gap-5 sm:ml-6 sm:flex sm:space-x-8">
                 <Link
                     to="/"
-                    className="text-black hover:text-black inline-flex items-center px-1 pt-1 font-medium"
+                    className="text-black hover:text-black inline-flex items-center px-1 pt-1 font-medium text-lg"
                 >
                     Home
                 </Link>
                 <Link
                     to="/how-it-works"
-                    className="text-[#4A154B] hover:text-[#4A154B] inline-flex items-center px-1 pt-1 font-medium"
+                    className="text-black hover:text-black inline-flex items-center px-1 pt-1 font-medium"
                 >
                     How It Works
                 </Link>
                 <Link
                     to="/about-skin-cancer"
-                    className="text-[#4A154B] hover:text-[#4A154B] inline-flex items-center px-1 pt-1 font-medium"
+                    className="text-black hover:text-black inline-flex items-center px-1 pt-1 font-medium"
                 >
                     About Skin Cancer
                 </Link>
                 <Link
                     to="/find-a-dermatologist"
-                    className="text-[#4A154B] hover:text-[#4A154B] inline-flex items-center px-1 pt-1 font-medium"
+                    className="text-black hover:text-black inline-flex items-center px-1 pt-1 font-medium"
                 >
                     Find a Dermatologist
                 </Link>
