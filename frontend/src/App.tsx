f8a439a9ec2b618f314bfa16b9f7045930507d1a
Sneha Sadhwani <snehasadhwani@hotmail.com>
2025-04-17 10:16:30 -0700

    added router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,34 @@
-import Home from "./pages/Home"
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
-function App() {
-  return <div><Home /></div>; 
+function Home() {
+    return <h1>Home</h1>;
 }
 
-export default App; 
+function HowItWorks() {
+    return <h1>How It Works</h1>;
+}
+
+function AboutSkinCancer() {
+    return <h1>About Skin Cancer</h1>;
+}
+
+function FindADermatologist() {
+    return <h1>Find a Dermatologist</h1>;
+}
+
+function App() {
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/how-it-works" element={<HowItWorks />} />
+                <Route path="/about-skin-cancer" element={<AboutSkinCancer />} />
+                <Route path="/find-a-dermatologist" element={<FindADermatologist />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
