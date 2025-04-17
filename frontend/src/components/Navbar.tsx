5555180c157f2b5ea2f978a0f3bbb8c4cb8508f4
Sneha Sadhwani <snehasadhwani@hotmail.com>
2025-04-17 10:42:26 -0700

    Changed color to black

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,35 +2,35 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
     return (
-        <div className="flex gap-5 h-16 w-full px-8 sm:px-6 lg:px-8 items-center">
+        <div className="flex gap-5 h-16 w-full px-8 sm:px-6 lg:px-8 items-center bg-[#F5F5F5]">
             <div className="flex items-center">
-                <i className="fas fa-dna text-indigo-600 text-2xl mr-2"></i>
-                <span className="text-xl font-bold text-gray-900">
+                <i className="fas fa-dna text-black  text-2xl mr-2"></i>
+                <span className="text-xl font-bold text-black">
                     DermaDetect
                 </span>
             </div>
             <div className="w-full flex justify-baseline gap-5 sm:ml-6 sm:flex sm:space-x-8">
                 <Link
                     to="/"
-                    className="border-indigo-500 text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 font-medium"
+                    className="text-black hover:text-black inline-flex items-center px-1 pt-1 font-medium"
                 >
-                    Home 
+                    Home
                 </Link>
                 <Link
                     to="/how-it-works"
-                    className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 font-medium"
+                    className="text-[#4A154B] hover:text-[#4A154B] inline-flex items-center px-1 pt-1 font-medium"
                 >
                     How It Works
                 </Link>
                 <Link
                     to="/about-skin-cancer"
-                    className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 font-medium"
+                    className="text-[#4A154B] hover:text-[#4A154B] inline-flex items-center px-1 pt-1 font-medium"
                 >
                     About Skin Cancer
                 </Link>
                 <Link
                     to="/find-a-dermatologist"
-                    className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 font-medium"
+                    className="text-[#4A154B] hover:text-[#4A154B] inline-flex items-center px-1 pt-1 font-medium"
                 >
                     Find a Dermatologist
                 </Link>
